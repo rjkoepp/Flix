20b4c3fb2c98b7a101e53c36c0d293eefb23cc4b
rjkoepp <rchangko@ucsd.edu>
2018-02-05 23:30:57 -0800

    Finished adding indicator

--- FlixApp/NowPlayingViewController.swift
+++ FlixApp/NowPlayingViewController.swift
@@ -12,18 +12,32 @@ import AlamofireImage
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var refreshControl: UIRefreshControl!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var movies: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         self.tableView.rowHeight = 200
-
-        
         // view controller is data source
         tableView.dataSource = self
-        
+        fetchMovies()
+
+    }
+    
+    // common for controls to pass selves
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        activityIndicator.startAnimating()
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
         // force unwrap
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
@@ -33,24 +47,26 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         
         // when our network request returns, it will jump back on our main    thread
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-     
+        
         // closure: <#T##(Data?, URLResponse?, Error?) -> Void#>
         
         let task = session.dataTask(with: request) { (data, response, error) in
             // this will run when the network reqeust returns, (asyncronous)
             if let error = error {
-                 print(error.localizedDescription)
+                print(error.localizedDescription)
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                
+                self.activityIndicator.stopAnimating()
+
             }
         }
         
         task.resume()
-        
-        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
