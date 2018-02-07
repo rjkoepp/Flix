96e9c6b9dbbf314a4846c0466d1d8e06c963bc41
rjkoepp <rchangko@ucsd.edu>
2018-02-06 17:05:00 -0800

    added navigation controllers and tab views

--- FlixApp/ViewControllers/DetailViewController.swift
+++ FlixApp/ViewControllers/DetailViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+enum MovieKeys {
+    static let title = "title"
+    static let releaseDate = "release_date"
+    static let overview = "overview"
+    static let backdropPath = "backdrop_path"
+    static let posterPath = "poster_path"
+}
+
 class DetailViewController: UIViewController {
 
     
@@ -26,9 +34,21 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
 
         if let movie = movie{
-            titleLabel.text = movie["title"] as? String
-            releaseDateLabel.text = movie["release_date"] as? String
-            overViewLabel.text = movie["overview"] as? String 
+            titleLabel.text = movie[MovieKeys.title] as? String
+            releaseDateLabel.text = movie[MovieKeys.releaseDate] as? String
+            overViewLabel.text = movie[MovieKeys.overview] as? String
+            let backdropPathString = movie[MovieKeys.backdropPath] as! String
+            let posterPathString = movie[MovieKeys.posterPath] as! String
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            
+            let backDropURL = URL(string: baseURLString + backdropPathString)!
+            
+            backDropImageView.af_setImage(withURL: backDropURL)
+
+            let posterPathURL = URL(string: baseURLString + posterPathString)!
+            
+            posterImageView.af_setImage(withURL: posterPathURL)
+            
         }
         
     }
